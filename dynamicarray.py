26c4a95262e6214dff3c891db99e0b2ff7bca36f
Rahul <rahulrajendran1098@gmail.com>
2023-08-25 13:06:46 +0530

    commented new dynamic array script

--- dynamicarray.py
+++ dynamicarray.py
@@ -1,47 +1,105 @@
 import ctypes
+import sys
 
-class DynamicArray(object):
-    
-    #
-    def __init__(self):
-        
-        self.no_of_elements = 0 #number of elements in the array
-        self.capacity = 1 # capacity of the array
+# there are two arrays the A array which is the initial array with data and B is the array that extends the A array
+
+
+class DynamicArray:
+
+    def __init__(self):  # create a dynamic array instance
+
+        self.n = 0  # no of elements held in the array. 
+        # we start with zero
+        # current capacity of the array. we start with one and it grows with each element
+        self.capacity = 1
+        # make the actual array and assign it to the A(inital array)
         self.A = self.make_array(self.capacity)
-        
-    def __len__(self):
-        return self.no_of_elements
-    
-    def __getitem__(self, index):
-         
-        if not 0 <= index < self.no_of_elements:
-            return IndexError('Index out of bounds')
-        return self.A[index] 
-    
-    def append(self, element):
-        if self.no_of_elements == self.capacity:
-            self._resize(2*self.capacity) #2x if capacity is reached
-        
-        self.A[self.no_of_elements] = element
-        self.no_of_elements += 1
-        
-    def _resize(self, new_capacity):
-        
-        B = self.make_array(new_capacity)
-        
-        for k in range(self.no_of_elements):
-            B[k] = self.A[k]
+
+    def __len__(self):  # just return the size of the array when len(x) is called
+
+        return self.n
+
+    def __getitem__(self, k):
+        """since we are creating an array we need to access elements using indexing.
+        This is done by the __getitem__ special method. 
+        We check for index limits and then return fromt the A array.
+        k is the index the user is indexing
+        Input -> k
+        return -> element from A at kth index 
+         """
+
+        if not 0 <= k < self.n:
+            return IndexError("k is out of bounds")
+        else:
+            return self.A[k]
+
+    def append(self, value):
+
+        if self.n == self.capacity:
+            # when we are appending we check if the current number of elements is at capacity.
+            # if yes then we resize the array
+
+            self._resize(2*self.capacity) #_resize is a private function
             
-        self.A = B
-        self.capacity = new_capacity
+        self.A[self.n] = value # assign it at the last position.
         
-    def make_array(self, new_capacity):
+        self.n += 1 #increase the number of elements in the array
         
-        return (new_capacity * ctypes.py_object)()
+    def _resize(self, new_cap): #dynamic increase of array size
+        
+        B = self.make_array(new_cap) 
+        # B is the new array that is twice the cap of the original array and gets copied to
+        
+        for k in range(self.n):
+            
+            B[k] = self.A[k] # copy the elements of original array to extended array
+            
+        self.A = B # make this the original array
+        self.capacity = new_cap #update the capacity of the new array    
+        
+    def make_array(self, new_cap):
+        
+        return (new_cap * ctypes.py_object)()
     
-if __name__ == '__main__':
+    
+    
+if __name__ == "__main__":
+    
     arr = DynamicArray()
-    arr.append(1)
-    print(len(arr))
-    arr.append(7)
-    print(arr[1])
+    arr.append(8)
+    print(arr.capacity)
+    # print(sys.getsizeof(arr))
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    arr.append(3)
+    arr.append(9)
+    print(arr.capacity)
+    arr.append(5)
+    arr.append(8)
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    arr.append(8)
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    arr.append(8)
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    arr.append(3)
+    arr.append(9)
+    arr.append(5)
+    print(arr.n)
+    # print(sys.getsizeof(arr))
+    for num, k in enumerate(range(len(arr))):
+        print(num + 1,arr[k])
+    
+    
